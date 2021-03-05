c4022f2eec553afb220ec39e8998032e08b4bd0a
SebasRios34 <ligasebasrios@gmail.com>
2021-03-04 21:17:05 -0600

    3/4/2021 9:16 PM - added ZIP File to app and shows txt values to input values

--- EReviewProgram/ConfigurationValues.xaml.cs
+++ EReviewProgram/ConfigurationValues.xaml.cs
@@ -23,10 +23,13 @@ namespace EReviewProgram
         public ConfigurationValues()
         {
             InitializeComponent();
-            extractFields();
+            extractServiceAccountPassword();
+            extractServiceExtAuthEndpoint();
+            extractInstallationDirectory();
         }
 
         string folderPath = InstallationDirectory.PropertiesFile + "\\install-assets\\install.properties";
+        string ereviewPath = InstallationDirectory.PropertiesFile + "\\tomcat\\ereview.properties";
 
         public void editServiceAccountPassword() 
         {
@@ -39,7 +42,7 @@ namespace EReviewProgram
                     readFile.Close();
                     using (StreamWriter writeFile = new StreamWriter(folderPath))
                     {
-                        string value = "serviceAccountPassword=" + txtServiceAccountPassword.Text + "\nserviceExtAuthEndpoint=";
+                        string value = "serviceAccountPassword=" + txtServiceAccountPassword.Text + "\nserviceExtAuthEndpoint=" + "\ninstallDir=";
                         writeFile.WriteLine(value);
                     }
                 }
@@ -61,7 +64,7 @@ namespace EReviewProgram
                         i++;
                         using (StreamWriter writeFile = new StreamWriter(folderPath))
                         {
-                            string value = "serviceAccountPassword=" + txtServiceAccountPassword.Text + "\nserviceExtAuthEndpoint=" + txtServiceExtAuthEndpoint.Text;
+                            string value = "serviceAccountPassword=" + txtServiceAccountPassword.Text + "\nserviceExtAuthEndpoint=" + txtServiceExtAuthEndpoint.Text + "\ninstallDir=" + InstallationDirectory.PropertiesFile;
                             writeFile.WriteLine(value);
                         }
                     }
@@ -73,21 +76,97 @@ namespace EReviewProgram
             };
         }
 
-        public void extractFields() 
+        public void editInstallDir() 
         {
-            string txtFile = File.ReadAllText(folderPath);
-            int firstPosition = txtFile.IndexOf("\n");
+            using (StreamReader readFile = new StreamReader(folderPath))
+            {
+                int i = 0;
 
-            string extractServiceAccountPassword = txtFile.Substring(0, firstPosition);
-            string extractServiceExtAuthEndpoint = txtFile.Substring(firstPosition + 1);
+                while (i == 0)
+                {
+                    string searchLine = readFile.ReadLine();
+                    if (searchLine.Contains("installDir"))
+                    {
+                        readFile.Close();
+                        i++;
+                        using (StreamWriter writeFile = new StreamWriter(folderPath))
+                        {
+                            string value = "serviceAccountPassword=" + txtServiceAccountPassword.Text + "\nserviceExtAuthEndpoint=" + txtServiceExtAuthEndpoint.Text + "\ninstallDir=" + InstallationDirectory.PropertiesFile;
+                            writeFile.WriteLine(value);
+                        }
+                    }
+                    else
+                    {
+                        i = 0;
+                    }
+                }
+            };
+        }
+        
+        /*
+        public void searchReplace() 
+        {
+            using (StreamReader readFile = new StreamReader(folderPath))
+            {
+                int i = 0;
 
-            string serviceAccountPassword = extractServiceAccountPassword.Remove(0, 23);
-            string serviceExtAuthEndpoint = extractServiceExtAuthEndpoint.Remove(0, 23);
+                while (i == 0) 
+                {
+                    string searchLine = readFile.ReadLine();
+                    if (searchLine.Contains("serviceAccountPassword"))
+                    {
+                        readFile.Close();
+                        i++;
+                        using (StreamWriter writeFile = new StreamWriter(ereviewPath)) 
+                        {
 
-            txtServiceAccountPassword.Text = serviceAccountPassword;
-            txtServiceExtAuthEndpoint.Text = serviceExtAuthEndpoint;
+                        }
+                    }
+                }
+            }
+        }
+        */
 
-            //MessageBox.Show("Password: " + serviceAccountPassword + "\nAuth Endpoint: " + serviceExtAuthEndpoint);
+        public void extractServiceAccountPassword() 
+        {
+            string toSearch = @"serviceAccountPassword";
+            string[] lines = File.ReadAllLines(folderPath);
+            foreach (string line in lines)
+            {
+                if (line.Contains(toSearch))
+                {
+                    string serviceAccountPassword = line.Remove(0, 23);
+                    txtServiceAccountPassword.Text = serviceAccountPassword;
+                }
+            }
+        }
+
+        public void extractServiceExtAuthEndpoint() 
+        {
+            string toSearch = @"serviceExtAuthEndpoint";
+            string[] lines = File.ReadAllLines(folderPath);
+            foreach (string line in lines)
+            {
+                if (line.Contains(toSearch))
+                {
+                    string serviceExtAuthEndpoint = line.Remove(0, 23);
+                    txtServiceExtAuthEndpoint.Text = serviceExtAuthEndpoint;
+                }
+            }
+        }
+
+        public void extractInstallationDirectory()
+        {
+            string toSearch = @"installDir";
+            string[] lines = File.ReadAllLines(folderPath);
+            foreach (string line in lines)
+            {
+                if (line.Contains(toSearch))
+                {
+                    string installDir = line.Remove(0, 11);
+                    txtInstallDir.Text = InstallationDirectory.PropertiesFile;
+                }
+            }
         }
 
         private void btnConfigure_Click(object sender, RoutedEventArgs e)
@@ -107,6 +186,7 @@ namespace EReviewProgram
             {
                 editServiceAccountPassword();
                 editServiceExtAuthEndpoint();
+                editInstallDir();
             }
 
             //ServicesInstalled page = new ServicesInstalled();
